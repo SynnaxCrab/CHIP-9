0c466c11e263517bf5a85ee53e467f3974037ed1
Jeff Lai <winfield301@gmail.com>
2020-05-05 01:12:08 -0700

    Read current opcode in memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -14,3 +14,24 @@ struct Cpu {
     // delayed timer
     dt: u8,
 }
+
+impl Cpu {
+    fn new() -> Cpu {
+        Cpu {
+            i: 0,
+            pc: 0,
+            memory: [0; 4096],
+            v: [0: 16],
+            stack: [0, 16],
+            sp: 0,
+            dt: 0
+        }
+    }
+
+    fn current_opcode() -> u16 {
+        let hi = self.memory[pc] as u16;
+        let lo = self.memory[pc + 1] as u16;
+
+        hi << 8 | lo
+    }
+}
