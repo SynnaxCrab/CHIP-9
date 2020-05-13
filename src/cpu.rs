ac4cf0fe5508722333c199583b3eb8838321b92d
Jeff Lai <winfield301@gmail.com>
2020-05-12 18:12:51 -0700

    Implement some of the opcodes

--- src/cpu.rs
+++ src/cpu.rs
@@ -36,7 +36,12 @@ impl Cpu {
     }
 
     fn process_opcode(&mut self) {
+        let x = (current_opcode() & 0x0F00) >> 8;
+        let y = (current_opcode() & 0x00F0) >> 4;
+        let vx = self.v[x];
+        let vy = self.v[y];
         let nnn = opcode & 0x0FFF;
+        let nn = opcode & 0x00FF;
 
         // break up into nibbles
         let op_1 = (current_opcode() & 0xF000) >> 12;
@@ -47,6 +52,45 @@ impl Cpu {
         self.pc += 2;
 
         match (op_1, op_2, op_3, op_4) {
+            // clears the display
+            (0, 0, 0xE, 0) => self.display.cls(),
+            // returns from a subroutine
+            (0, 0, 0xE, 0xE) => {
+                self.sp = self.sp - 1;
+                self.pc = self.stack[self.sp as usize];
+            },
+            // Jumps to address
+            (0x1, _, _, _) => self.pc = nnn,
+            // Calls subroutine
+            (0x2, _, _, _) => {
+                self.stack[self.sp as usize] = self.pc;
+                self.sp += 1;
+                self.pc = nnn;
+            },
+            // Skips the next instruction if VX equals NN
+            (0x3, _, _, _) => {
+                if vx == nn {
+                    self.pc += 2;
+                }
+            },
+            // Skips the next instruction if VX doesn't equal NN
+            (0x4, _, _, _) => {
+                if vx != nn {
+                    self.pc += 2;
+                }
+            },
+            // Skips the next instruction if VX equals VY
+            (0x5, _, _, _) => {
+                if vx = vy {
+                    self.pc += 2;
+                }
+            },
+            // Set VX to NN
+            (0x6, _, _, _) => self.v[x] = nn,
+            // Adds NN to VX (Carry flag is not changed)
+            (0x7, _, _, _) => self.v[x] += nn,
+            // Sets VX to the value of VY.
+            (0x8, _, _, 0x1) => self.v[x] = v[y],
             (0xA, _, _, _) => self.i = nnn,
         }
     }
