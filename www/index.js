1d60ef48958179f032ad54764acc2818d2fe1220
Jeff Lai <winfield301@gmail.com>
2020-08-17 23:33:17 -0700

    Add basic canvas

--- www/index.js
+++ www/index.js
@@ -1,3 +1,9 @@
 import * as wasm from "chip-9";
 
+const canvas = document.getElementById("chip-8");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "green";
+ctx.fillRect(10, 10, 150, 100);
+
 wasm.cpu();
