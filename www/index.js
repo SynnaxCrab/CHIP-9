f431d8063fd97d440819565309bc271f88eb1e8a
Jeff Lai <winfield301@gmail.com>
2020-08-19 23:41:18 -0700

    Draw the black background of canvas

--- www/index.js
+++ www/index.js
@@ -1,45 +1,14 @@
 import * as wasm from "chip-9";
 
-const canvas = document.getElementById("chip-8");
-const ctx = canvas.getContext("2d");
+const run = async () => {
+  const WIDTH = 64;
+  const HEIGHT = 32;
+  const canvas = document.getElementById("chip-8");
+  const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
-ctx.fillStyle = "rgb(200, 0, 0)";
-ctx.fillRect(20, 20, 50, 50);
-
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.fillRect(30, 30, 50, 50);
-
-ctx.beginPath();
-ctx.moveTo(75, 50);
-ctx.lineTo(100, 75);
-ctx.lineTo(100, 25);
-ctx.fill();
-
-ctx.beginPath();
-ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-ctx.moveTo(110, 75);
-ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-ctx.moveTo(65, 65);
-ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-ctx.moveTo(95, 65);
-ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
-ctx.stroke();
-
-// Filled triangle
-ctx.beginPath();
-ctx.moveTo(25, 25);
-ctx.lineTo(105, 25);
-ctx.lineTo(25, 105);
-ctx.fill();
-
-// Stroked triangle
-ctx.beginPath();
-ctx.moveTo(125, 125);
-ctx.lineTo(125, 45);
-ctx.lineTo(45, 125);
-ctx.closePath();
-ctx.stroke();
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+};
 
 wasm.cpu();
+run();
