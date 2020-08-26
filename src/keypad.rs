63fac117635df4a65ba2692cf2390508455bf820
Jeff Lai <winfield301@gmail.com>
2020-08-25 23:55:57 -0700

    Impl is_key_down

--- src/keypad.rs
+++ src/keypad.rs
@@ -13,7 +13,11 @@ impl Keypad {
         self.keys[index as usize] = true;
     }
 
-    pub fn key_up(&mut self, index: u8) {
+    fn key_up(&mut self, index: u8) {
         self.keys[index as usize] = false;
+    }
+
+    pub fn is_key_down(&self, index: u8) -> bool {
+        self.keys[index as usize]
     }
 }
